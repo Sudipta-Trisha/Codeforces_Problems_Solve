3f5bd113467b42fc3c0db3944111504ed2cc9aa5
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2019-11-05 19:05:45 +0600

    Number Theory

---
+++ 630A. Again Twenty Five!.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    cin>>n;
+
+    cout<<"25"<<endl;
+    return 0;
+}
