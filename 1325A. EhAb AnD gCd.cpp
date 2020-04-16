f717bd811d3eae1c29307104b34bd5da49ecdba2
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2020-04-16 12:15:03 +0600

    Number Theory, Greedy

---
+++ 1325A. EhAb AnD gCd.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--){
+        int x;
+        cin>>x;
+        cout<<"1"<<" "<<x-1<<endl;
+    }
+    return 0;
+}
