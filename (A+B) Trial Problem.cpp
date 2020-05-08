2827a77080d417713e571747c15a10761e1ddcc9
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2020-05-08 08:15:17 -0700

    Implementation , Math

---
+++ (A+B) Trial Problem.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long int 
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    ll sum=0;
+    while(t--){
+        ll a,b;
+        cin>>a>>b;
+        sum = a+b;
+        cout<<sum<<endl;
+    }
+}
