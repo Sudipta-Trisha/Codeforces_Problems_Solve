8f824d115deadbf2197b5c3acfda6a4a8edf8752
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2020-07-04 21:15:38 +0600

    Simple math & implementation

---
+++ 1370A. Maximum GCD.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+	int test;
+	cin >> test;
+	
+	while(test--){
+		ll n;
+		cin >> n;
+		
+		cout << n/2 <<endl;
+	}
+	return 0;
+}
