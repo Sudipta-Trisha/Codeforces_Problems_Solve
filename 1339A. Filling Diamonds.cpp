b0bba2222b57c1193135b4c611b2bb20f75bcd70
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2021-02-10 20:03:19 +0600

    Add files via upload

---
+++ 1339A. Filling Diamonds.cpp
@@ -0,0 +1,22 @@
+
+// Problem: A. Filling Diamonds
+// Contest: Codeforces - Codeforces Round #633 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1339/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int test;
+	cin >> test;
+	while(test--){
+		int n;
+		cin >> n;
+		
+		cout << n << endl;
+	}
+}
