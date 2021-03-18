eda3649c304809b95a0650e0e3ec8bf835ba608e
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2021-03-18 12:30:31 +0600

    Add files via upload

---
+++ 1455A. Strange Functions.cpp
@@ -0,0 +1,23 @@
+
+// Problem: A. Strange Functions
+// Contest: Codeforces - Educational Codeforces Round 99 (Rated for Div. 2)
+// URL: https://codeforces.com/problemset/problem/1455/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int test;
+	cin >> test;
+	while(test--){
+		string s;
+		cin >> s;
+		
+		int len = s.length();
+		
+		cout << len << endl;
+	}
+}
