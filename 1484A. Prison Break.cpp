d7463efdfa3f324f9e76061dcc8d610a4743b240
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2021-03-22 00:56:17 +0600

    Add files via upload

---
+++ 1484A. Prison Break.cpp
@@ -0,0 +1,22 @@
+
+// Problem: A. Prison Break
+// Contest: Codeforces - Codeforces Round #709 (Div. 2, based on Technocup 2021 Final Round)
+// URL: https://codeforces.com/contest/1484/problem/0
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+	ll test;
+	cin >> test;
+	while(test--){
+		int a,b;
+		cin >> a >> b;
+		cout << a*b << endl;
+	}
+}
