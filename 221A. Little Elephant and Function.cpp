79c20a4c206fbb4349aa67d43cfdec7c8210d27d
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2021-03-26 20:33:05 +0600

    Add files via upload

---
+++ 221A. Little Elephant and Function.cpp
@@ -0,0 +1,22 @@
+
+// Problem: A. Little Elephant and Function
+// Contest: Codeforces - Codeforces Round #136 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/221/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ll n;
+	cin >> n;
+	
+	cout << n << " ";
+	for(ll i=1; i<n; i++){
+		cout << i << " ";
+	}
+	cout << endl;
+}
